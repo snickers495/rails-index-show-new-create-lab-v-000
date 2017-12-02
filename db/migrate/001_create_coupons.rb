ebd2f559c5f4784c05b551b5aeaa784ef607f05e
Lisa Lee <snickers495@gmail.com>
2017-12-02 17:58:26 +0000

    automatically backed up by learn

--- db/migrate/001_create_coupons.rb
+++ db/migrate/001_create_coupons.rb
@@ -1,7 +1,7 @@
 class CreateCoupons < ActiveRecord::Migration
   def change
     create_table :coupons do |c|
-      c.integer :coupon_code
+      c.text :coupon_code
       c.text :store
 
       c.timestamps null: false
